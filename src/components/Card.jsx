0b072831c589e3f147e0f955f532c2f7fd38892b
Samson Ameh <amehsam69@gmail.com>
2023-12-14 13:08:14 +0100

    file setup done

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <h1>This is the cards</h1>
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./public/img/logo.png" alt="my travel journal logo" />
+      <h3>my travel journal.</h3>
+    </header>
+  );
+}
